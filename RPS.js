7ff34513453c3156fee115dfd80ef7fa0f51ef71
Dedit-ops <audacious401@gmail.com>
2024-11-22 21:09:15 -0800

    Fix bug and added playround and playgame function after I added my playround and playgame function, I had a bug that returned an incorrect result after each round. I fixed it here

--- RPS.js
+++ RPS.js
@@ -2,44 +2,101 @@
 // write a code to allow human enter numbers and return rock or paper or scissors
 
                       
+let x = 'rock'
+let y = 'paper'
+let z = 'scissors'
 
 function getComputerChoice () {
     let game = Math.floor((Math.random() * 3)); 
 
     if (game === 0) {
-        return console.log('Rock');
+         return x;
     }
     else if (game === 1) {
-        return console.log('Paper');
+         return y;
     }
-    else {
-        return console.log('Scissors');
+    else if (game === 2){
+         return z;
      
     }
+   
 
 }
-console.log(getComputerChoice ())
 
-function getHumanChoice (choose) {
+
+function getHumanChoice () {
+    let choose = prompt('Enter your choice').toLowerCase();
+
+     if (choose === x || choose === y || choose === z) {
+         return choose;
+    }
+    else{alert ('invalid choice! please enter rock paper or scissors');
+        return getHumanChoice();
+    }
     
 
-    if (choose === 0) {
-        return console.log('Rock');
+ }
+
+
+    function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice)  {
+        return `It's a tie! You both chose ${humanChoice}.`;
     }
-    else if (choose === 1) {
-        return console.log('Paper');
+    else if (
+        (humanChoice === x && computerChoice === z) ||
+        (humanChoice === y && computerChoice === x) || 
+        (humanChoice === z && computerChoice === y)
+    ) {
+        
+        return `You win! ${humanChoice} beats ${computerChoice}.`;
     }
     else {
-        return console.log('Scissors');
-     
+        
+        return `You lose! ${computerChoice} beats ${humanChoice}.`;
     }
 
- }
-let userChoice = parseInt(prompt('Enter a number (0 for rock, 1 for paper, 2 for scissors:'))
+    
+  } 
 
- console.log(getHumanChoice (userChoice))
 
-//     
+ 
   
 
 
+
+function playGame () {
+    let humanScore = 0
+    let computerScore = 0
+
+    for(i=1; i<=5; i++){
+        console.log(`Round ${i}`);
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
+        let result = playRound(humanChoice, computerChoice);
+        console.log(result);
+        if (result.includes('You win')) {
+            humanScore++;
+        } else if (result.includes('You lose')) {
+            computerScore++;
+        }
+
+        console.log(`Score after Round ${i}: You ${humanScore} - ${computerScore} Computer`);
+
+        if (humanScore > computerScore) {
+            console.log(`You won the game! Final score: You ${humanScore} - ${computerScore} Computer`);
+        } else if (humanScore < computerScore) {
+            console.log(`You lost the game. Final score: You ${humanScore} - ${computerScore} Computer`);
+        } else {
+            console.log(`The game is a tie! Final score: You ${humanScore} - ${computerScore} Computer`);
+        }
+
+
+        
+    }
+    
+
+
+}
+playGame();
